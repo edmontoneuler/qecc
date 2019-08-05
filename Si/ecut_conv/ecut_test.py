c86eab1466375357906dba8788c3fe75b843475d
Cameron Rudderham <cmrudder@beluga3.int.ets1.calculquebec.ca>
2019-08-05 13:07:19 -0400

    Succesful test of ecut_test.py

--- Si/ecut_conv/ecut_test.py
+++ Si/ecut_conv/ecut_test.py
@@ -1,26 +1,26 @@
 import sys
 import os
-sys.path.insert(0, "/home/cmrudder/scratch/bin")
 
-from QL_struct_makers import make_Bi2Te3_QL
+sys.path.insert(0, "/home/cmrudder/scratch/qecc/bin")
+from diamond_struct_makers import make_bulk_Si
 from kpts_makers import make_auto_kpts
 from param_makers import make_scf_param
 from bash_makers import make_bash_beluga
 
-values = [5*k +40 for k in range(12)]
-make_auto_kpts('KPTS', grid = [11,11,1,0,0,0])
-make_Bi2Te3_QL('STRUCT')
+values = [5*k +30 for k in range(8)]
+make_auto_kpts('KPTS', grid = [11,11,11,0,0,0])
+make_bulk_Si('STRUCT')
 N = len(values)
 
 for k in range(N):
     iteration = 'ecutwfc' + str(values[k])
     os.system('mkdir ' + iteration)
-    make_scf_param('PARAMS', iteration, pseudo_dir = '../../PP/', ecutwfc = values[k], ecutrho = 4*values[k])
+    make_scf_param('PARAMS', pseudo_dir = '../../PP/', ecutwfc = values[k], ecutrho = 4*values[k], nat = 2, ntyp = 1, nbnd = 16)
     os.system('cat PARAMS STRUCT KPTS >> scf.in')
     os.system('dos2unix scf.in')
     os.system('mv scf.in  ' + iteration)
 
-    make_bash_beluga('scf.sh', job_name = 'Bi2Te3_QL' +'_' + iteration, time = '00:20:00')
+    make_bash_beluga('scf.sh', job_name = 'bulk_Si_' + iteration, time = '00:20:00')
     os.system('mv scf.sh ' + iteration)
 
     os.chdir(iteration)
