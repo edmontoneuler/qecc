075a4bfaf394eb999df451970f8484aa09cab1e7
Cameron Rudderham <cmrudder@beluga3.int.ets1.calculquebec.ca>
2019-08-05 15:49:20 -0400

    initial commit get_dos.pyinitial commit get_dos.py

---
+++ Si/dos/get_dos.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+from bash_makers import make_bash_beluga 
+
+f = open('dos.in', 'w+')
+f.write(' &DOS\n')
+f.write("    prefix = 'GS'\n")
+f.write("    outdir = './'\n")
+f.write("    filband = 'dos.dat'\n")
+f.write("    Emin=-10, Emax = 30.0, DeltaE =0.02\n")
+f.write(' /') 
+f.close()
+
+os.system('dos2unix bands.in')
+
+make_bash_beluga('dos.sh',executable = 'dos.x', qe_inputfile = 'dos.in', qe_outputfile = 'dos.out', job_name = 'get_Si_dos', time = '00:30:00')
+os.system('sbatch dos.sh')
+
+
+
