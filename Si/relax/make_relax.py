56fec3871cb0741433a75227ee1afb4b249f8963
Cameron Rudderham <cmrudder@beluga4.int.ets1.calculquebec.ca>
2019-08-06 19:42:51 -0400

    Successful test of make_relax with randomized atomic coordinates

--- Si/relax/make_relax.py
+++ Si/relax/make_relax.py
@@ -1,19 +1,19 @@
 import sys
 import os
-sys.path.insert(0, "/scratch/m/maassenj/cmrudder/bin")
 
-from zincblende_struct_makers import make_bulk_GaAs
+sys.path.insert(0, "/home/cmrudder/scratch/qecc/bin")
+from diamond_struct_makers import make_bulk_Si
 from kpts_makers import make_auto_kpts
 from param_makers import make_relax_param
-from bash_makers import make_bash_niagara 
+from bash_makers import make_bash_beluga 
 
 make_auto_kpts('KPTS', grid = [11,11,11,0,0,0])
-make_bulk_GaAs('STRUCT')
-make_relax_param('PARAMS', ecutwfc = 60, ecutrho = 720, nat = 2, ntyp = 2, nbnd = 16)
+make_bulk_Si('STRUCT', rand_disp = 0.001) #Random displacement of atomic coordinates
+make_relax_param('PARAMS', ecutwfc = 60, ecutrho = 720, nat = 2, ntyp = 1, nbnd = 16)
 os.system('cat PARAMS STRUCT KPTS >> relax.in')
 os.system('dos2unix relax.in')
 
-make_bash_niagara('relax.sh',qe_inputfile = 'relax.in',qe_outputfile = 'relax.out', job_name = 'GaAs_relaxation', time = '02:00:00')
+make_bash_beluga('relax.sh',qe_inputfile = 'relax.in',qe_outputfile = 'relax.out', job_name = 'Si_relaxation', time = '02:00:00')
 os.system('sbatch relax.sh')
 
 os.system('rm -f PARAMS STRUCT KPTS')
