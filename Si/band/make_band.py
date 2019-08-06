4c7e231df7e3a7442a89dc6906e4fa36540cf483
Cameron Rudderham <cmrudder@beluga4.int.ets1.calculquebec.ca>
2019-08-06 17:26:28 -0400

    fix path to bin in make_band.py

--- Si/band/make_band.py
+++ Si/band/make_band.py
@@ -1,8 +1,8 @@
 import sys
 import os
-sys.path.insert(0, "/scratch/m/maassenj/cmrudder/bin")
 
-from zincblende_struct_makers import make_bulk_Si
+sys.path.insert(0, "/home/cmrudder/scratch/qecc/bin")
+from diamond_struct_makers import make_bulk_Si
 from kpts_makers import make_diamond_bands
 from param_makers import make_scf_param
 from bash_makers import make_bash_beluga 
@@ -13,7 +13,7 @@ make_scf_param('PARAMS', conv_thr = 1e-10,calculation = 'bands', ecutwfc = 60, e
 os.system('cat PARAMS STRUCT KPTS >> band.in')
 os.system('dos2unix band.in')
 
-make_bash_beluga('band.sh', qe_inputfile = 'band.in', qe_outputfile = 'band.out', job_name = 'Si_bands', time = '00:30:00')
+make_bash_beluga('band.sh', qe_inputfile = 'band.in', qe_outputfile = 'band.out', job_name = 'Si_band', time = '00:30:00')
 os.system('sbatch band.sh')
 
 os.system('rm -f PARAMS STRUCT KPTS')
