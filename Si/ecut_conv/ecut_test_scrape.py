cc8810b39bd7c4d0662b13e0f1e487324a1261f9
Cameron Rudderham <cmrudder@beluga4.int.ets1.calculquebec.ca>
2019-08-06 19:45:47 -0400

    improve ecut_test_scrape functionality

--- Si/ecut_conv/ecut_test_scrape.py
+++ Si/ecut_conv/ecut_test_scrape.py
@@ -8,24 +8,20 @@ from scf_scrape import get_scf_dict
 values = [5*k +30 for k in range(8)]
 
 total_energies = []
-data_dir = 'test_data'     #Name of directory to store output files
-os.system('mkdir ' + data_dir)
 N = len(values)
 
 for k in range(N):
     iteration = 'ecutwfc' + str(values[k])
-    os.chdir(iteration)
     
-    os.system('cp scf.out ../' + data_dir + '/' + iteration +'.out') 
+    os.chdir(iteration)
     scf_dict = get_scf_dict('scf.out', iteration + '_pickle') 
     total_energies.append(scf_dict['total_energy'])
     
     os.chdir('..')
-    os.system('rm -rf ' + iteration)
 
 
-conv_plot_data = np.column_stack((values, total_energies))
-np.savetxt('conv_plot_data', conv_plot_data)
-os.system('mv conv_plot_data ' + data_dir + '/')
+ecut_test_data = np.column_stack((values, total_energies))
+np.savetxt('ecut_test_data', ecut_test_data)
+
 print('ecutwfc values = ', values)
 print('Total energies = ', total_energies)
