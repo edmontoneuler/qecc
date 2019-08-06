80b6918bdc1a86626ef660aa953f0537fa1518f2
Cameron Rudderham <cmrudder@beluga3.int.ets1.calculquebec.ca>
2019-08-06 10:35:38 -0400

    initial commit of get_dos.py

--- Si/dos/get_dos.py
+++ Si/dos/get_dos.py
@@ -1,6 +1,7 @@
 import sys
 import os
 
+sys.path.insert(0, "/home/cmrudder/scratch/qecc/bin")
 from bash_makers import make_bash_beluga 
 
 f = open('dos.in', 'w+')
@@ -12,7 +13,7 @@ f.write("    Emin=-10, Emax = 30.0, DeltaE =0.02\n")
 f.write(' /') 
 f.close()
 
-os.system('dos2unix bands.in')
+os.system('dos2unix dos.in')
 
 make_bash_beluga('dos.sh',executable = 'dos.x', qe_inputfile = 'dos.in', qe_outputfile = 'dos.out', job_name = 'get_Si_dos', time = '00:30:00')
 os.system('sbatch dos.sh')
