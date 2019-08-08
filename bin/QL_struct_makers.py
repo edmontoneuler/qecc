9a26568f35ddd34d45210d9a8654bdbd7ff8284b
Cameron Rudderham <cmrudder@beluga3.int.ets1.calculquebec.ca>
2019-08-08 19:50:11 -0400

    updated QL structure maker to incorporate random displacements

--- bin/QL_struct_makers.py
+++ bin/QL_struct_makers.py
@@ -1,11 +1,26 @@
 import numpy as np
 import sys 
+import random
 
-def make_Bi2Te3_QL(struc_filename, a = 4.383, vac_layer = 15, D1 = 1.70, D2 = 2.05):
+def make_Bi2Te3_QL(struc_filename, a = 4.383, vac_layer = 15, D1 = 1.70, D2 = 2.05, rand_disp = 0):
     c = vac_layer + 2*D1 + 2*D2
     IL1 = D1/c
     IL2 = D2/c
-    
+
+    #Atomic coordinates (x-y fixed by hex symmetries, inter-layer spacing variable)
+    Te2a_coords = np.array([2/3, 1/3, IL1+IL2])
+    Bi1a_coords = np.array([1/3, 2/3, IL1])
+    Te1_coords = np.array([0, 0, 0])
+    Bi1b_coords = np.array([2/3, 1/3, -IL1])
+    Te2b_coords = np.array([1/3, 2/3, -IL1 - IL2])
+
+    #Randomly displace atomic coordinates
+    Te2a_coords = Te2a_coords + rand_disp*np.array([random.uniform(0,1) for k in range(3)])
+    Bi1a_coords = Bi1a_coords+ rand_disp*np.array([random.uniform(0,1) for k in range(3)])
+    Te1_coords = Te1_coords #+ rand_disp*np.array([random.uniform(0,1) for k in range(3)])
+    Bi1b_coords = Bi1b_coords + rand_disp*np.array([random.uniform(0,1) for k in range(3)])
+    Te2b_coords = Te2b_coords + rand_disp*np.array([random.uniform(0,1) for k in range(3)])
+
     #Lattice vectors (Hexagonal)
     v1 = [a, 0, 0]
     v2 = [-0.5*a, 0.5*np.sqrt(3)*a, 0]
@@ -25,18 +40,18 @@ def make_Bi2Te3_QL(struc_filename, a = 4.383, vac_layer = 15, D1 = 1.70, D2 = 2.
     f.write('\n')
 
     f.write('ATOMIC_POSITIONS (crystal)\n')
+    f.write('Te    ' + str(Te2a_coords[0]) + ' ' +  str(Te2a_coords[1]) +  ' ' +  str(Te2a_coords[2]) +'\n')
+    f.write('Bi    ' + str(Bi1a_coords[0]) + ' ' +  str(Bi1a_coords[1]) +  ' ' +  str(Bi1a_coords[2]) +'\n')
+    f.write('Te    ' + str(Te1_coords[0]) + ' ' +  str(Te1_coords[1]) +  ' ' +  str(Te1_coords[2]) +'\n')
+    f.write('Bi    ' + str(Bi1b_coords[0]) + ' ' +  str(Bi1b_coords[1]) +  ' ' +  str(Bi1b_coords[2]) +'\n')
+    f.write('Te    ' + str(Te2b_coords[0]) + ' ' +  str(Te2b_coords[1]) +  ' ' +  str(Te2b_coords[2]) +'\n')
+    
+    #f.write('ATOMIC_POSITIONS (crystal)\n')
     #f.write('Te  0.0000000  0.0000000 0.0000000\n')
     #f.write('Bi  0.6666667  0.3333333 ' +str(IL1) +'\n')
     #f.write('Te  0.3333333  0.6666667 ' +str(IL1 +IL2) +'\n')
     #f.write('Bi  0.0000000  0.0000000 ' +str(IL1 +2*IL2) +'\n')
     #f.write('Te  0.6666667  0.3333333 ' +str(2*IL1 +2*IL2) +'\n')
-
-    f.write('Te  0.0000000  0.0000000 -0.002538209\n')
-    f.write('Bi  0.6666667  0.3333333  0.075469618\n')
-    f.write('Te  0.3333333  0.6666667  0.166666667\n')
-    f.write('Bi  0.0000000  0.0000000  0.257863715\n')
-    f.write('Te  0.6666667  0.3333333  0.335871542\n')
-
 
     f.close()
 
