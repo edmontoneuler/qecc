d64a74c10ac6271940477760a5251b0eb7eba0e3
edmontoneuler <cameron.rudderham@gmail.com>
2019-08-08 22:16:30 -0300

    Added Ry2eV conversion to plotting scripts

--- Si/ecut_conv/plot_ecut_conv.py
+++ Si/ecut_conv/plot_ecut_conv.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = np.loadtxt('ecut_test_data')
+Ry2eV = 13.60569562
+tolerance = 0.001 # 1meV
 
 ecutwfc = data[:, 0]
 total_E = data[:, 1]
@@ -10,7 +12,10 @@ delta_E = np.zeros((len(total_E)-1, 1))
 for k in range(len(delta_E)):
     delta_E[k] = np.abs(total_E[-1] - total_E[k])
 
-plt.plot(ecutwfc[0:-1], delta_E)
+plt.plot(ecutwfc[0:-1], Ry2eV*delta_E)
+plt.plot((ecutwfc[0], ecutwfc[-2]), (tolerance, tolerance), 'r--')
 plt.xlabel('ecutwfc (Ry)')
-plt.ylabel('Difference in total energy (Ry)')
+plt.ylabel('Difference in total energy (eV)')
 plt.show()
+
+
--- Si/kpt_conv/plot_kpt_conv.py
+++ Si/kpt_conv/plot_kpt_conv.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = np.loadtxt('kpt_test_data')
+Ry2eV = 13.60569562
+tolerance = 0.001 # 1 meV
 
 kpt_num = data[:, 0]
 total_E = data[:, 1]
@@ -10,7 +12,8 @@ delta_E = np.zeros((len(total_E)-1, 1))
 for k in range(len(delta_E)):
     delta_E[k] = np.abs(total_E[-1] - total_E[k])
 
-plt.plot(kpt_num[0:-1], delta_E)
+plt.plot(kpt_num[0:-1], Ry2eV*delta_E)
+plt.plot((kpt_num[0], kpt_num[-2]), (tolerance, tolerance), 'r--')
 plt.xlabel('K-point grid [X, X, X]')
-plt.ylabel('Difference in total energy (Ry)')
+plt.ylabel('Difference in total energy (eV)')
 plt.show()
